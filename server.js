0e4b5bf4616c8ea679edc3b325c14d374baa5dd6
Kartik <kartikagarwal20144@gmail.com>
2024-06-25 23:54:50 +0530

    correcting / bug

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
 
 app.use("/api/blog", blogRoutes);
 app.use("/api/users", userRoutes);
